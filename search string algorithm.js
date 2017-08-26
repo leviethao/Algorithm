e9d452b0a22edc5cf97705d1323c71911b3466c7
leviethao <leviethao2@gmail.com>
2017-08-26 16:13:19 +0700

    Update search string algorithm.js

--- search string algorithm.js
+++ search string algorithm.js
@@ -1,3 +1,18 @@
+/*
+ALGORITHM DESCRIPTION
+Xét chuỗi nguồn (từ input) và 1 mảng các chuỗi đích(chuỗi có sẵn trong database)
+-Giả sử mỗi chuỗi đích có 1 id để phân biệt
+-Tạo 1 mảng mới chứa các phần tử là các cặp key-value. Trong đó key là id của các chuỗi đích, 
+value là chỉ số để đánh giá mức độ gần đúng của chuỗi đích này so với chuỗi nguồn.
+ vd: var arr = [ {id: xxx, core: yyy} ];
+- Tính độ gần đúng (tính core)
+	+ Tách chuỗi nguồn thành mảng các từ (giả sử ta đặt tên cho mảng này là wordArr)
+	+ Duyệt qua từng chuỗi đích, tính tổng số lần xuất hiện của mỗi từ trong wordArr có trong
+	chuỗi đích, chia tổng tính được cho độ dài chuỗi đích.
+	+ Lưu tổng tính được vào thuộc tính core
+- Trong mảng arr, id có core lớn nhất là chuỗi gần đúng nhất
+*/
+
 
 //============= ALGORITHM ===========
 
